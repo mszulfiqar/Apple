2da04d10d2313045e6d83519a77f7a2857dd907c
sz-khan <mshanzulfiark@gmail.com>
2023-10-23 12:39:23 -0700

    Added more features in topnav

---
+++ j.js
@@ -0,0 +1,9 @@
+var store = document.querySelector(".store");
+var body = document.querySelector("body");
+
+// store.addEventListener("mouseenter",function(){
+// 	// body.style.backgroundColor = "green";
+// });
+// store.addEventListener("mouseleave",function(){
+// 	// body.style.backgroundColor = "yellow";
+// });
